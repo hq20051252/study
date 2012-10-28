d9319cabd0644c2e6fd7ddf9dae79492260f4575
hq20051252 <hq20051252@163.com>
2012-10-28 21:24:22 +0800

    permutation,remove the 'limit'

--- permutation.py
+++ permutation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
-''''''
-
+#--*--coding:utf8--*--
+'''要修改，使得空间是O(1)的，'''
+global permu
+permu=[]
 def _permutation(invertation):
     '''Construct a permutation from 1 to N,by the invertation table of
         a permutation.'''
     if(isinstance(invertation, list)):
         length=len(invertation)
         permu=[]
-
         for i in range(0, length):
             permu.insert(invertation[i], length-i)
-        print permu
+#        print permu
     else:
         print "Type error"
 
@@ -20,38 +21,45 @@ def gerninvertation(scale):
     invertation=[]
     for i in range(scale):
         invertation.append(0)
+    yield invertation
+    try:
+        while True:
+            invertation=add(invertation,1)
+            yield invertation
+    except FlowOverError:
+        print "It's over.!"
 
-    limit=reduce(int.__mul__,range(1,scale+1))
+
+#    limit=reduce(lambda x,y:x*y,range(1,scale+1))
 #    print limit
-    i=1
-    while i<=limit:
-        if i==1:
-            i=i+1
-            yield invertation
-        else:
-            try:
-                invertation=add(invertation,1)
-            except FlowOverError:
-                print "It's over.!"
-            i=i+1
-            yield invertation
+#    i=1
+#    while i<=limit:
+#        if i==1:
+#            i=i+1
+#
+#        else:
+#            try:
+#                invertation=add(invertation,1)
+#            except FlowOverError:
+#                print "It's over.!"
+#            i=i+1
+#            yield invertation
 
 
-def add(invertationtable,lens=1):
+def add(invertation,lens=1):
     '''An inversed table is seen as a number,but on the i-th bit i-th number
     system,we emulate the add operation.'''
-    invertation=invertationtable
     length=len(invertation)
-#    print length
-    #we need the following gaurd variable.The 'flag' marks the state whether we
-    #should stop calculating.'index' mark which bit we are.forward marks how much
-    #we should carrying forward.
+
+#we need the following gaurd variable.The 'flag' marks the state whether we
+#should stop calculating.'index' mark which bit we are.forward marks how much
+#we should carrying forward.
     flag=0
     index=0
     forward=1
-    #if addition has done (flag==0),stop.
+#if addition has done (flag==0),stop.
     while flag==0 and (index < length):
-#        print index
+
         if invertation[index]+forward < index+1:
             invertation[index]=invertation[index]+forward
             flag=1
@@ -67,17 +75,17 @@ def add(invertationtable,lens=1):
 
 def permutation(scale):
     generator=gerninvertation(scale)
-    while True:
-        invertation=generator.next()
-        _permutation(invertation)
+    try:
+        while True:
+            invertation=generator.next()
+            _permutation(invertation)
+    except StopIteration:
+        print "permutation has print over."
+        return 0
 
 class FlowOverError(BaseException):
     def __init__(self):
         print "FlowOverError"
 
 if __name__=='__main__':
-#    permutation(15)
-#    invertation=[0,1,2,3]
-#    print add(invertation,1)
-    while True:
-        permutation(5)
+    permutation(23)
